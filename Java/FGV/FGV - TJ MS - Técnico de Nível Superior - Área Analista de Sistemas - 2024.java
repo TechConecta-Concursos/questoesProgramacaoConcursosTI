69b9e81a3fca1d32d9c745928b387fc05c016043
Carlos André <68868602+opacarlos@users.noreply.github.com>
2024-11-10 19:46:46 -0300

    Create FGV - TJ MS - Técnico de Nível Superior - Área Analista de Sistemas - 2024.java

---
+++ Java/FGV/FGV - TJ MS - Técnico de Nível Superior - Área Analista de Sistemas - 2024.java
@@ -0,0 +1,14 @@
+// Enunciado:
+// Considere a seguinte função na linguagem Java:
+
+public static int funcao(int x, int y) { // Função que recebe dois inteiros e retorna ela mesma. Esse assunto se chama Recursividade.
+  if (y == 0) // Se o inteiro y for igual a 0, então...
+    return x; // Inteiro x será retornado
+  else // Caso não...
+    return funcao(y, x % y); // A função será chamada até y ser igual a 0
+}
+
+// O retorno da chamada funcao(49, 72) é:
+//A) 1  B) 2  C) 3  D) 7  E) 9
+
+// Resolução:
