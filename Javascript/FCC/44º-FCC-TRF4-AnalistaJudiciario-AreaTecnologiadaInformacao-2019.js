bfd13d2b0e4d4283ee2362577d8a5779f40a2795
Eduardo Vítor <eduardovitor730@gmail.com>
2025-01-21 21:18:27 -0300

    Add questao JS TRF4 FCC - falta adicionar comentarios

---
+++ Javascript/FCC/44º-FCC-TRF4-AnalistaJudiciario-AreaTecnologiadaInformacao-2019.js
@@ -0,0 +1,22 @@
+/*
+Link (Questões Gran Cursos): https://questoes.grancursosonline.com.br/questoes-de-concursos/tecnologia-da-informacao-14/2095186
+
+Considere o fragmento de programa JavaScript abaixo.
+
+A variável resultado vai conter
+
+*/
+
+var str = "123456789";
+
+var p = /[^5-7]/g;
+
+var resultado = str.match(p);
+
+/*
+
+Comentário humano ...
+
+
+Comentário por IA ...
+*/
