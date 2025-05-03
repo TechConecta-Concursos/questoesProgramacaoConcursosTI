aa2b62035b50eb1ab4efc930646e3a6f611a31fd
Carlos André <68868602+opacarlos@users.noreply.github.com>
2025-05-03 15:58:40 -0300

    Create 108º-CEBRASPE-2025-TRF6-AnalistaJudiciário-AnalisedeDados.java

---
+++ Java/Cebraspe/108º-CEBRASPE-2025-TRF6-AnalistaJudiciário-AnalisedeDados.java
@@ -0,0 +1,16 @@
+/*
+Enunciado:
+
+Julgue o próximo item, relativo a Java. 
+
+import java.io.*; 
+class Easy{
+  public static void main(String[] args){
+    int x[] = {1, 3, 7, 22, 51};
+    for (int size : x)
+      System.out.print(size);
+  }
+}
+
+O resultado da execução do código precedente, desenvolvido em Java, será 6. 
+*/
