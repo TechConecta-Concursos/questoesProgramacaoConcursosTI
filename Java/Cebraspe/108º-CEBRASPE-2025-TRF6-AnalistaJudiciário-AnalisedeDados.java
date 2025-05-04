6e6437182b01f708872d34ce0e223d43e8b3b963
Carlos André <68868602+opacarlos@users.noreply.github.com>
2025-05-04 15:34:10 -0300

    Update 108º-CEBRASPE-2025-TRF6-AnalistaJudiciário-AnalisedeDados.java

--- Java/Cebraspe/108º-CEBRASPE-2025-TRF6-AnalistaJudiciário-AnalisedeDados.java
+++ Java/Cebraspe/108º-CEBRASPE-2025-TRF6-AnalistaJudiciário-AnalisedeDados.java
@@ -1,7 +1,8 @@
 /*
 Enunciado:
 
-Julgue o próximo item, relativo a Java. 
+Julgue o próximo item, relativo a Java.
+*/
 
 import java.io.*; 
 class Easy{
@@ -12,5 +13,22 @@ class Easy{
   }
 }
 
+/*
 O resultado da execução do código precedente, desenvolvido em Java, será 6. 
+
+C) Certo
+E) Errado
+
+Comentário humano:
+Temos aqui uma questão relativamente fácil de lógica de programação em Java, não é à toa que o nome da classe é Easy.
+De início nós temos os padrões comuns de um algoritmo Java, tendo como nosso trecho de interesse apenas o que se encontra dentro
+do método main. Nele nós temos a declaração de um vetor com 5 elementos e depois um laço de repetição FOR que repete por 5 vezes
+(que é o tamanho do vetor x) a seguinte linha: "System.out.print(size);".
+Daí, nós já podemos perceber que o resultado não será 6, pois o FOR fará com que 5 elementos diferentes sejam mostrados. São eles:
+1372251 //Saída
+
+Obs: Como algumas pessoas podem ter percebido, esse for está meio "diferente", pois se trata de um enhanced for loop (ou for-each).
+A diferença dele para o FOR "comum" é que ele não precisa de um índice para correr todo o vetor.
+
+Resposta: E) Errado
 */
